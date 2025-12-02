b8539b1d43ed81609baa49f00796b55957928ea6
Pushpindersingh-2027 <pushpindersingh0203@gmail.com>
2025-12-02 19:32:30 +1100

    Full upload of all SIT725 files from D:\\sit725

---
+++ 1.3/script.js
@@ -0,0 +1,17 @@
+// Simple JavaScript Program for SIT725 Task 1.3P
+
+// A message
+console.log("Hello, this is SIT725 Task 1.3P!");
+
+// A variable
+let number = 5;
+
+// A simple function
+function square(x) {
+    return x * x;
+}
+
+// Output results
+console.log("The number is:", number);
+console.log("The square of the number is:", square(number));
+console.log("Program completed successfully.");
